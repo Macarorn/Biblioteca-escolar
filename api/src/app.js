96e5876eb1636e0609508b64a593f202cb2f7d82
Macarron <marianacifuenteszu@gmail.com>
2026-02-11 11:25:39 -0500

    conexion a base de datos

--- api/src/app.js
+++ api/src/app.js
@@ -1,7 +1,8 @@
-import express from "express";
 import cors from "cors";
-import morgan from "morgan";
 import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+import db from "./config/database.js";
 
 dotenv.config();
 
@@ -13,6 +14,25 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "API Biblioteca Escolar activa 📚" });
+});
+
+//Para probar que la conexión es exitosa probar http://localhost:3000/db-check
+app.get("/db-check", async (req, res) => {
+  try {
+    await db.query("SELECT 1");
+    res.json({
+      connected: true,
+      message: "Conexión exitosa a la base de datos",
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        connected: false,
+        message: "Error de conexión a la base de datos",
+        error: error.message,
+      });
+  }
 });
 
 export default app;
